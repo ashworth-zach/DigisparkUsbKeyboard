59b45ffd0c0b2e0173bd6914fa2a15353df6483d
Zach Ashworth <zashworth2@gmail.com>
2018-11-05 21:27:42 -0600

    fixes

--- police_bar.ino
+++ police_bar.ino
@@ -1,13 +1,7 @@
 #include <Adafruit_NeoPixel.h>
 #define PIN 6
 #define NUM_LEDS 60
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB + NEO_KHZ800);
 void setup()
 {
@@ -17,9 +11,6 @@ void setup()
 }
 
 void loop() { 
-  // Slower:
-  // Strobe(0xff, 0x77, 0x00, 10, 100, 1000);
-  // Fast:
   Strobe(127, 127, 127, 800, 50, 1000);
 }
 
